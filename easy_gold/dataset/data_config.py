bde5deca256d02d46981b355d44951ebbe6ca45c
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2023-10-22 11:57:11 +0900

    docs: add refered url

--- easy_gold/dataset/data_config.py
+++ easy_gold/dataset/data_config.py
@@ -2,11 +2,13 @@ from pathlib import Path
 
 
 class MINDConfig:
+    # refs: https://learn.microsoft.com/ja-jp/azure/open-datasets/dataset-microsoft-news?tabs=azureml-opendatasets
     download_base_url = "https://mind201910small.blob.core.windows.net/release"
     training_small_url = f"{download_base_url}/MINDsmall_train.zip"
     validation_small_url = f"{download_base_url}/MINDsmall_dev.zip"
     training_large_url = f"{download_base_url}/MINDlarge_train.zip"
     validation_large_url = f"{download_base_url}/MINDlarge_dev.zip"
+
     parent_dir = Path("/kaggle/input/mind-news-dataset")
     news_tsv = parent_dir.joinpath("news.tsv/news.tsv")
     behaviors_tsv = parent_dir.joinpath("MINDsmall_train/behaviors.tsv")
