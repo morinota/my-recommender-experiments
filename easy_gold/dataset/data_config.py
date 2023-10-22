24baacb02d020a11793412cf876fa21a9f067246
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2023-10-22 11:55:55 +0900

    feat: add download url for MIND

--- easy_gold/dataset/data_config.py
+++ easy_gold/dataset/data_config.py
@@ -1,7 +1,12 @@
 from pathlib import Path
 
 
-class MINDPathConfig:
+class MINDConfig:
+    download_base_url = "https://mind201910small.blob.core.windows.net/release"
+    training_small_url = f"{download_base_url}/MINDsmall_train.zip"
+    validation_small_url = f"{download_base_url}/MINDsmall_dev.zip"
+    training_large_url = f"{download_base_url}/MINDlarge_train.zip"
+    validation_large_url = f"{download_base_url}/MINDlarge_dev.zip"
     parent_dir = Path("/kaggle/input/mind-news-dataset")
     news_tsv = parent_dir.joinpath("news.tsv/news.tsv")
     behaviors_tsv = parent_dir.joinpath("MINDsmall_train/behaviors.tsv")
