928967dbea3d454e102ecff79bf3d78b8b54687d
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2023-11-29 22:43:57 +0900

    feat: ConvertRawInputToAtomicTaskの単体テストと実装を追加した。

--- my_recommender_experiments/recommender_experiments/model/data_flow/convert_raw_input_to_atomic.py
+++ my_recommender_experiments/recommender_experiments/model/data_flow/convert_raw_input_to_atomic.py
@@ -1,48 +1,25 @@
-import os
-from pathlib import Path
-import tempfile
-
-import zipfile
-import gokart
-
+import pandas as pd
 
 from recommender_experiments.model.task_interface import TaskInterface
-from recommender_experiments.model.data_flow.download_raw_input import DownloadRawInputTask
-from recommender_experiments.dataset.MIND_dataset import MINDDataset
 
 
 class ConvertRawInputToAtomicTask(TaskInterface):
-    zip_path = gokart.TaskInstanceParameter()
+    SEPARATOR = "\t"
 
-    def __init__(self, raw_input_zip_path: Path) -> None:
-        self.raw_input_zip_path = raw_input_zip_path
+    def __init__(self) -> None:
+        pass
 
-    def run(self) -> tuple[Path, Path, Path, Path]:
-        zip_path: Path = self.load("zip_path")
-        output_path = Path(tempfile.gettempdir()) / "mind"
-        output_path.mkdir(parents=True, exist_ok=True)
-        dataset_kind = "validation_small"
-        return self._run(zip_path, output_path, dataset_kind)
+    def run(
+        self,
+        df: pd.DataFrame,
+        field_type_by_id: dict[int, str],
+    ) -> str:
+        output_data = pd.DataFrame()
+        for col_idx in field_type_by_id:
+            output_data[col_idx] = df.iloc[:, col_idx]
 
-    @staticmethod
-    def _run(
-        zip_path: Path,
-        output_path: Path,
-        dataset_kind: str,
-    ) -> tuple[Path, Path, Path, Path]:
-        temp_dir = Path(tempfile.gettempdir()) / "mind"
-        with zipfile.ZipFile(zip_path, "r") as zip_ref:
-            zip_ref.extractall(temp_dir)
-
-        dataset = MINDDataset(temp_dir, output_path, dataset_kind)
-
-        behaviors_path = dataset.convert_behaviors()
-        news_path = dataset.convert_news()
-        entity_embedding_path = dataset.convert_entity_embedding()
-        relation_embedding_path = dataset.convert_relation_embedding()
-        return (
-            behaviors_path,
-            news_path,
-            entity_embedding_path,
-            relation_embedding_path,
-        )
+        # 1行目はfield名
+        atomic_str = self.SEPARATOR.join([field_type_by_id[int(col_idx)] for col_idx in output_data.columns]) + "\n"
+        for record in output_data.itertuples():
+            atomic_str += self.SEPARATOR.join([str(record[int(col_idx) + 1]) for col_idx in output_data.columns]) + "\n"
+        return atomic_str
---
+++ my_recommender_experiments/test/unit_test/test_convert_raw_input_to_atomic.py
@@ -0,0 +1,28 @@
+from recommender_experiments.model.data_flow.convert_raw_input_to_atomic import ConvertRawInputToAtomicTask
+import pandas as pd
+
+
+def test_df_is_converted_str_for_atomic_file() -> None:
+    # Arrange
+    df = pd.DataFrame(
+        {
+            "id": [1, 2],
+            "category": ["cat1", "cat2"],
+            "subcategory": ["subcat1", "subcat2"],
+        }
+    )
+    field_type_by_id = {
+        0: "id:token",
+        1: "category:token",
+        2: "subcategory:token",
+    }
+    sut = ConvertRawInputToAtomicTask()
+
+    # Act
+    atomic_data = sut.run(df, field_type_by_id)
+    print("\n")
+    print(atomic_data)
+
+    # Assert
+    atomic_data_expected = "id:token\tcategory:token\tsubcategory:token\n1\tcat1\tsubcat1\n2\tcat2\tsubcat2\n"
+    assert atomic_data == atomic_data_expected
