632805050ac8a5e2ab449b944422f467c21f0b6b
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2023-12-01 22:26:02 +0900

    feat: 単体テストと実装を追加した。

--- my_recommender_experiments/recommender_experiments/dataset/MIND_dataset.py
+++ my_recommender_experiments/recommender_experiments/dataset/MIND_dataset.py
@@ -1,159 +1,164 @@
+from dataclasses import dataclass
 import os
 from pathlib import Path
+import tempfile
 from typing import Optional
+import zipfile
 from numpy import int8
 
 import pandas as pd
-from recommender_experiments.dataset.base_dataset import BaseDataset
+from recommender_experiments.dataset.base_dataset import RawDatasetInterface
 
 
-class MINDDataset(BaseDataset):
-    BASE_NAME = "MIND"
+@dataclass
+class MINDDataset(RawDatasetInterface):
+    # selected feature fields
+    # 型について -> https://recbole.io/docs/user_guide/data/atomic_files.html#format
     DATASET_KINDS_CANDIDATES = [
         "training_small",
         "validation_small",
         "training_large",
         "validation_large",
     ]
+    RAW_FILES_INFO = {
+        "behaviors": {
+            "filename": "behaviors.tsv",
+            "sep": "\t",
+        },
+        "news": {
+            "filename": "news.tsv",
+            "sep": "\t",
+        },
+        "entity_embeddings": {
+            "filename": "entity_embedding.vec",
+            "sep": "\t",
+        },
+        "relation_embeddings": {
+            "filename": "relation_embedding.vec",
+            "sep": "\t",
+        },
+    }
 
-    def __init__(self, input_path: Path, output_path: Path, dataset_kind: str) -> None:
-        if dataset_kind not in self.DATASET_KINDS_CANDIDATES:
-            raise ValueError(f"`dataset_kind` argument need to be in {self.DATASET_KINDS_CANDIDATES}")
+    behaviors_fields = {
+        0: "impression_id:token",
+        1: "user_id:token",
+        2: "time:float",
+        3: "history:token_seq",
+        4: "item_id:token",
+        5: "is_tap:float",
+    }
+    news_fields = {
+        0: "id:token",
+        1: "category:token",
+        2: "subcategory:token",
+        3: "title:token_seq",
+        4: "abstract:token_seq",
+        5: "url:token",
+        6: "title_entities:token_seq",
+        7: "abstract_entities:token_seq",
+    }
+    entity_embedding_fields = {
+        0: "entity_id:token",
+        1: "vector:float_seq",
+    }
+    relation_embedding_fields = {
+        0: "relation_id:token",
+        1: "vector:float_seq",
+    }
 
-        self.input_path = input_path
-        self.output_path = output_path
-        self.check_output_path()
-        self.dataset_name = f"{self.BASE_NAME}_{dataset_kind}"
+    behaviors: pd.DataFrame
+    news: pd.DataFrame
+    entity_embeddings: pd.DataFrame
+    relation_embeddings: pd.DataFrame
 
-        # input_path
-        self.behavior_file = self.input_path / "behaviors.tsv"
-        self.news_file = self.input_path / "news.tsv"
-        self.entity_embedding_file = self.input_path / "entity_embedding.vec"
-        self.relation_embedding_file = self.input_path / "relation_embedding.vec"
-        self.sep = "\t"
+    @classmethod
+    def load_from_zip(cls, zip_path: Path) -> "MINDDataset":
+        """
+        - zip_pathのzipファイル内に、4つのファイルが圧縮されている。
+        - zipファイルをtemp directoryにunzipし、4つのファイルをpd.DataFrameとしてメモリに載せ、dataclassの各fieldに載せてdataclassとして初期化する。
+        """
+        unziped_dir = cls._unzip_to_temp_dir(zip_path)
 
-        # output_path
-        output_files = self._get_output_files()
-        self.output_behavior_file = output_files[0]
-        self.output_news_file = output_files[1]
-        self.output_entity_embedding_file = output_files[2]
-        self.output_relation_embedding_file = output_files[3]
+        behaviors = cls._load_behaviors_data(unziped_dir)
+        news = cls._load_news_data(unziped_dir)
+        entity_embeddings = cls._load_entity_embedding_data(unziped_dir)
+        relation_embeddings = cls._load_relation_embedding_data(unziped_dir)
 
-        # selected feature fields
-        # 型について -> https://recbole.io/docs/user_guide/data/atomic_files.html#format
-        self.behaviors_fields = {
-            0: "impression_id:token",
-            1: "user_id:token",
-            2: "time:float",
-            3: "history:token_seq",
-            4: "item_id:token",
-            5: "is_tap:float",
-        }
-        self.news_fields = {
-            0: "id:token",
-            1: "category:token",
-            2: "subcategory:token",
-            3: "title:token_seq",
-            4: "abstract:token_seq",
-            5: "url:token",
-            6: "title_entities:token_seq",
-            7: "abstract_entities:token_seq",
-        }
-        self.entity_embedding_fields = {
-            0: "entity_id:token",
-            1: "vector:float_seq",
-        }
-        self.relation_embedding_fields = {
-            0: "relation_id:token",
-            1: "vector:float_seq",
-        }
+        return MINDDataset(
+            behaviors,
+            news,
+            entity_embeddings,
+            relation_embeddings,
+        )
 
-    def _load_behaviors_data(self) -> pd.DataFrame:
+    @classmethod
+    def _unzip_to_temp_dir(cls, zip_path: Path) -> Path:
+        temp_dir = Path(tempfile.gettempdir()) / "mind"
+        temp_dir.mkdir(parents=True, exist_ok=True)
+
+        with zipfile.ZipFile(zip_path, "r") as zip_ref:
+            zip_ref.extractall(temp_dir)
+        return temp_dir
+
+    @classmethod
+    def _load_behaviors_data(cls, unziped_dir: Path) -> pd.DataFrame:
         behavior_df = pd.read_table(
-            self.behavior_file,
+            unziped_dir / cls.RAW_FILES_INFO["behaviors"]["filename"],
             header=None,
             names=["impression_id", "user_id", "time", "history", "impressions"],
         )
-        return self._expand_each_impression(behavior_df)
+        separator = ImpresionsSeparator()
+        return separator.separate(behavior_df, "impressions")
 
-    def _load_news_data(self) -> pd.DataFrame:
+    @classmethod
+    def _load_news_data(cls, unziped_dir: Path) -> pd.DataFrame:
         return pd.read_table(
-            self.news_file,
+            unziped_dir / cls.RAW_FILES_INFO["news"]["filename"],
             header=None,
             names=["id", "category", "subcategory", "title", "abstract", "url", "title_entities", "abstract_entities"],
         )
 
-    def _load_entity_embedding_data(self) -> pd.DataFrame:
-        entity_embedding = pd.read_table(self.entity_embedding_file, header=None)
+    @classmethod
+    def _load_entity_embedding_data(cls, unziped_dir: Path) -> pd.DataFrame:
+        entity_embedding = pd.read_table(
+            unziped_dir / cls.RAW_FILES_INFO["entity_embeddings"]["filename"],
+            header=None,
+        )
         entity_embedding["vector"] = entity_embedding.iloc[:, 1:101].values.tolist()
         entity_embedding = entity_embedding[[0, "vector"]].rename(columns={0: "entity_id"})
         return entity_embedding
 
-    def _load_relation_embedding_data(self) -> pd.DataFrame:
-        relation_embedding = pd.read_table(self.relation_embedding_file, header=None)
+    @classmethod
+    def _load_relation_embedding_data(cls, unziped_dir: Path) -> pd.DataFrame:
+        relation_embedding = pd.read_table(
+            unziped_dir / cls.RAW_FILES_INFO["relation_embeddings"]["filename"],
+            header=None,
+        )
         relation_embedding["vector"] = relation_embedding.iloc[:, 1:101].values.tolist()
         relation_embedding = relation_embedding[[0, "vector"]].rename(columns={0: "entity_id"})
         return relation_embedding
 
-    def _get_output_files(self) -> tuple[Path, Path, Path, Path]:
-        output_behavior_file = self.output_path / f"{self.dataset_name}_behaviors.inter"
-        output_news_file = self.output_path / f"{self.dataset_name}_news.item"
-        output_entity_embedding_file = self.output_path / f"{self.dataset_name}_entity_embedding.ent"
-        output_relation_embedding_file = self.output_path / f"{self.dataset_name}_relation_embedding.rel"
 
-        return (
-            output_behavior_file,
-            output_news_file,
-            output_entity_embedding_file,
-            output_relation_embedding_file,
-        )
+class ImpresionsSeparator:
+    def __init__(self) -> None:
+        pass
 
-    def convert_behaviors(self) -> Path:
-        input_behaviors_data = self._load_behaviors_data()
-        return self._convert(
-            input_behaviors_data,
-            self.behaviors_fields,
-            self.output_behavior_file,
-        )
+    def separate(
+        self,
+        behavior_df: pd.DataFrame,
+        impressions_col: str = "impressions",
+        separated_col: str = "news_id",
+    ) -> pd.DataFrame:
+        implicit_feedbacks = []
+        for _, row in behavior_df.iterrows():
+            user_id = row["user_id"]
+            impressions_str = row[impressions_col]
+            impressions_list = impressions_str.split()
 
-    def convert_news(self) -> Path:
-        input_news_data = self._load_news_data()
-        return self._convert(
-            input_news_data,
-            self.news_fields,
-            self.output_news_file,
-        )
+            for impression in impressions_list:
+                news_id, is_interact = impression.split("-")
+                if is_interact == "0":
+                    continue
+                implicit_feedbacks.append({"user_id": user_id, separated_col: news_id})
 
-    def convert_entity_embedding(self) -> Path:
-        input_entity_embedding_data = self._load_entity_embedding_data()
-        return self._convert(
-            input_entity_embedding_data,
-            self.entity_embedding_fields,
-            self.output_entity_embedding_file,
-        )
-
-    def convert_relation_embedding(self) -> Path:
-        input_relation_embedding_data = self._load_relation_embedding_data()
-        return self._convert(
-            input_relation_embedding_data,
-            self.relation_embedding_fields,
-            self.output_relation_embedding_file,
-        )
-
-    def _expand_each_impression(self, behavior_df: pd.DataFrame) -> pd.DataFrame:
-        # 各impressionを展開し、新しいDataFrameを作成
-        df_except_impressions = behavior_df.drop("impressions", axis=1)
-        expanded_df = (
-            behavior_df["impressions"]
-            .str.split(" ", expand=True)
-            .stack()
-            .reset_index(level=1, drop=True)
-            .to_frame("impression_list")
-            .join(df_except_impressions)
-        )
-        expanded_df[["item_id", "is_tap_str"]] = expanded_df["impression_list"].str.split("-", expand=True)
-
-        expanded_df["is_tap"] = expanded_df["is_tap_str"].astype(int)
-        # データが多すぎるのでis_tap=0のレコードを落とす
-        expanded_df_only_tap = expanded_df.loc[expanded_df["is_tap"] == 1]
-        return expanded_df[["impression_id", "user_id", "item_id", "history", "time", "is_tap"]]
+        return pd.DataFrame(implicit_feedbacks)
---
+++ my_recommender_experiments/test/unit_test/test_mind_dataset.py
@@ -0,0 +1,26 @@
+from recommender_experiments.dataset.MIND_dataset import ImpresionsSeparator
+import pandas as pd
+
+
+def test_impressions_are_separated_as_implicit_feedback() -> None:
+    behavior_df = pd.DataFrame(
+        {
+            "user_id": [1],
+            "impressions": ["N1-1 N2-1 N3-0 N4-0 N5-0"],
+        }
+    )
+    impressions_col = "impressions"
+    separated_col = "news_id"
+    sut = ImpresionsSeparator()
+
+    implicit_feedbacks = sut.separate(behavior_df, impressions_col, separated_col)
+
+    implicit_feedbacks_expected = behavior_df = pd.DataFrame(
+        {
+            "user_id": [1, 1],
+            "news_id": ["N1", "N2"],
+        }
+    )
+    assert type(implicit_feedbacks) == pd.DataFrame
+    assert len(implicit_feedbacks) == len(implicit_feedbacks_expected)
+    assert implicit_feedbacks.to_dict() == implicit_feedbacks_expected.to_dict()
