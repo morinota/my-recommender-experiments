95427db828258cf797825a4d33f32239a7a5cfdf
MasatoNagoya <morita.masato.s3@s.mail.nagoya-u.ac.jp>
2024-01-07 19:35:39 +0900

    recboleの挙動をFacadeクラスに隠した

--- training_pipelines/main.py
+++ training_pipelines/main.py
@@ -1,18 +1,21 @@
 from pathlib import Path
-from custom_models.nrms import NRMS
-from recbole.model.general_recommender import BPR
 from training_executor import TrainingExecutor
 from configs.recbole_config import load_config_from_yamls
+from atomic_coverter.atomic_file_fetcher import AtomicFileFetcher
 
 
 def main() -> None:
     # TODO: feature storeから学習データをfetchし、atomic fileに変換する処理をtraining pipelineに含めたい
+    dataset_name = "mind_training_small"
+    raw_data_dir = Path("./feature_store/raw_data/")
     atomic_files_dir = Path("./feature_store/atomic_data/")
+    atomic_file_fetcher = AtomicFileFetcher()
+    atomic_file_fetcher.fetch(raw_data_dir, atomic_files_dir, dataset_name)
+
     # current_dirを出力
     print(f"[LOG] current_dir: {Path.cwd()}")
 
     model = "BPR"
-    dataset_name = "mind_training_small"
     config_files_dir = Path("./training_pipelines/configs/BPR_configs/")
 
     config_dict = load_config_from_yamls(config_files_dir)
